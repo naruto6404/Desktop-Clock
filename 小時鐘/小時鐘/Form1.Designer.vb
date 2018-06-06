e60098b3663411c411a1c0dad66747c851311fc1
Angai <35431381+naruto6404@users.noreply.github.com>
2018-06-06 15:17:31 +0800

    Add files via upload

---
+++ 小時鐘/小時鐘/Form1.Designer.vb
@@ -0,0 +1,212 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意:  以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請不要使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox6 = New System.Windows.Forms.PictureBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.P2 = New System.Windows.Forms.PictureBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.P2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(32, Byte), Integer), CType(CType(49, Byte), Integer), CType(CType(51, Byte), Integer))
+        Me.Button1.Image = CType(resources.GetObject("Button1.Image"), System.Drawing.Image)
+        Me.Button1.Location = New System.Drawing.Point(221, 0)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(48, 48)
+        Me.Button1.TabIndex = 1
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(23, 63)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(25, 37)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(47, 63)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(25, 37)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox2.TabIndex = 3
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), System.Drawing.Image)
+        Me.PictureBox3.Location = New System.Drawing.Point(109, 63)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(25, 37)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox3.TabIndex = 4
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Image = CType(resources.GetObject("PictureBox4.Image"), System.Drawing.Image)
+        Me.PictureBox4.Location = New System.Drawing.Point(133, 63)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(25, 37)
+        Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox4.TabIndex = 5
+        Me.PictureBox4.TabStop = False
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.Image = CType(resources.GetObject("PictureBox5.Image"), System.Drawing.Image)
+        Me.PictureBox5.Location = New System.Drawing.Point(196, 63)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(25, 37)
+        Me.PictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox5.TabIndex = 6
+        Me.PictureBox5.TabStop = False
+        '
+        'PictureBox6
+        '
+        Me.PictureBox6.Image = CType(resources.GetObject("PictureBox6.Image"), System.Drawing.Image)
+        Me.PictureBox6.Location = New System.Drawing.Point(220, 63)
+        Me.PictureBox6.Name = "PictureBox6"
+        Me.PictureBox6.Size = New System.Drawing.Size(25, 37)
+        Me.PictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox6.TabIndex = 7
+        Me.PictureBox6.TabStop = False
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1000
+        '
+        'P2
+        '
+        Me.P2.Image = CType(resources.GetObject("P2.Image"), System.Drawing.Image)
+        Me.P2.Location = New System.Drawing.Point(0, 0)
+        Me.P2.Name = "P2"
+        Me.P2.Size = New System.Drawing.Size(269, 132)
+        Me.P2.TabIndex = 9
+        Me.P2.TabStop = False
+        '
+        'Button2
+        '
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(32, Byte), Integer), CType(CType(49, Byte), Integer), CType(CType(51, Byte), Integer))
+        Me.Button2.Image = CType(resources.GetObject("Button2.Image"), System.Drawing.Image)
+        Me.Button2.Location = New System.Drawing.Point(173, 0)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(48, 48)
+        Me.Button2.TabIndex = 10
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.FromArgb(CType(CType(32, Byte), Integer), CType(CType(49, Byte), Integer), CType(CType(51, Byte), Integer))
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(0, 11)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(104, 25)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "yy/mm/dd"
+        '
+        'Button3
+        '
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.ForeColor = System.Drawing.Color.FromArgb(CType(CType(32, Byte), Integer), CType(CType(49, Byte), Integer), CType(CType(51, Byte), Integer))
+        Me.Button3.Image = CType(resources.GetObject("Button3.Image"), System.Drawing.Image)
+        Me.Button3.Location = New System.Drawing.Point(126, 0)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(48, 48)
+        Me.Button3.TabIndex = 12
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(269, 125)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox6)
+        Me.Controls.Add(Me.PictureBox5)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.P2)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.Text = "Clock"
+        Me.TopMost = True
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.P2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox3 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox4 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox5 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox6 As System.Windows.Forms.PictureBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents P2 As System.Windows.Forms.PictureBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button3 As Button
+End Class
---
+++ 小時鐘/小時鐘/Form1.vb
@@ -0,0 +1,210 @@
+﻿Public Class Form1
+
+    Dim hr, min, sec As Integer
+    Dim pic As Image() = {My.Resources.n0, My.Resources.n1, My.Resources.n2, My.Resources.n3, My.Resources.n4, My.Resources.n5, My.Resources.n6, My.Resources.n7, My.Resources.n8, My.Resources.n9}
+    Dim l, drag As Boolean
+    Dim l2, x1, y, dragging_x, dragging_y As Integer
+    Dim lo As String()
+    Dim h12 As Boolean
+    Dim tmbtn_me As Boolean
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        timeshow()
+        If My.Computer.FileSystem.FileExists("C:\Windows\ClockConfig.cfg") = True Then
+            lo = Split(My.Computer.FileSystem.ReadAllText("C:\Windows\ClockConfig.cfg"), ",")
+            Me.Location = New Point(lo(0), lo(1))
+            If lo(2) = "True" Then
+                h12 = True
+                Button2.Image = My.Resources.h12
+            Else
+                h12 = False
+                Button2.Image = My.Resources.h24
+            End If
+            If Me.TopMost Then
+                Button3.Image = My.Resources.topmost_btn_on
+            Else
+                Button3.Image = My.Resources.topmost_btn
+            End If
+        End If
+    End Sub
+
+    Sub timeshow()
+        hr = Now.Hour : min = Now.Minute : sec = Now.Second
+
+        Label1.Text = Now.Year & "/" & Now.Month & "/" & Now.Day
+
+        If h12 = True Then
+            If hr > 12 Then
+                hr -= 12
+            ElseIf hr = 0 Then
+                hr = 12
+            End If
+        End If
+
+        If hr < 10 Then
+            PictureBox1.Image = pic(0)
+        Else
+            PictureBox1.Image = pic(Strings.Left(hr, 1))
+        End If
+
+        PictureBox2.Image = pic(Strings.Right(hr, 1))
+
+        If min < 10 Then
+            PictureBox3.Image = pic(0)
+        Else
+            PictureBox3.Image = pic(Strings.Left(min, 1))
+        End If
+
+        PictureBox4.Image = pic(Strings.Right(min, 1))
+
+        If sec < 10 Then
+            PictureBox5.Image = pic(0)
+        Else
+            PictureBox5.Image = pic(Strings.Left(sec, 1))
+        End If
+
+        PictureBox6.Image = pic(Strings.Right(sec, 1))
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        timeshow()
+
+        If tmbtn_me Then
+            If Me.TopMost Then
+                Button3.Image = My.Resources.topmost_btn_on
+            Else
+                Button3.Image = My.Resources.topmost_btn
+            End If
+        End If
+
+        bar_hidden()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Button1.Image = My.Resources.Close_btn_press
+        Me.Close()
+    End Sub
+
+    Private Sub Button1_MouseDown(sender As Object, e As MouseEventArgs) Handles Button1.MouseDown
+        Button1.Image = My.Resources.Close_btn_press
+    End Sub
+
+    Private Sub Button1_MouseUp(sender As Object, e As MouseEventArgs) Handles Button1.MouseUp
+        Button1.Image = My.Resources.Close_btn
+        Me.Close()
+    End Sub
+
+    Private Sub Button1_MouseEnter(sender As Object, e As EventArgs) Handles Button1.MouseEnter
+        tmbtn_me = True
+        Button1.Image = My.Resources.Close_btn_hover
+    End Sub
+
+    Private Sub Button1_MouseLeave(sender As Object, e As EventArgs) Handles Button1.MouseLeave
+        tmbtn_me = True
+        Button1.Image = My.Resources.Close_btn
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Me.TopMost = Not Me.TopMost
+        If Me.TopMost Then
+            Button3.Image = My.Resources.topmost_btn_on
+        Else
+            Button3.Image = My.Resources.topmost_btn
+        End If
+    End Sub
+
+    Private Sub Button3_MouseEnter(sender As Object, e As EventArgs) Handles Button3.MouseEnter
+        Button3.Image = My.Resources.topmost_btn_hover
+        tmbtn_me = True
+    End Sub
+
+    Private Sub Button3_MouseLeave(sender As Object, e As EventArgs) Handles Button3.MouseLeave
+        If Me.TopMost Then
+            Button3.Image = My.Resources.topmost_btn_on
+        Else
+            Button3.Image = My.Resources.topmost_btn
+        End If
+        tmbtn_me = False
+    End Sub
+
+    Private Sub P2_MouseDown(sender As Object, e As MouseEventArgs) Handles P2.MouseDown, PictureBox1.MouseDown, PictureBox2.MouseDown, PictureBox3.MouseDown, PictureBox4.MouseDown, PictureBox5.MouseDown, PictureBox6.MouseDown, Label1.MouseDown
+        If e.Button = Windows.Forms.MouseButtons.Left Then
+            drag = True
+            dragging_x = e.X
+            dragging_y = e.Y
+        End If
+    End Sub
+
+    Private Sub P2_MouseMove(sender As Object, e As MouseEventArgs) Handles P2.MouseMove, PictureBox1.MouseMove, PictureBox2.MouseMove, PictureBox3.MouseMove, PictureBox4.MouseMove, PictureBox5.MouseMove, PictureBox6.MouseMove, Label1.MouseMove
+        If drag = True Then
+            Me.Left += e.X - dragging_x
+            Me.Top += e.Y - dragging_y
+        End If
+        bar_hidden()
+    End Sub
+
+    Private Sub P2_MouseUp(sender As Object, e As MouseEventArgs) Handles P2.MouseUp, PictureBox1.MouseUp, PictureBox2.MouseUp, PictureBox3.MouseUp, PictureBox4.MouseUp, PictureBox5.MouseUp, PictureBox6.MouseUp, Label1.MouseUp
+        If e.Button = Windows.Forms.MouseButtons.Left Then
+            If Me.Top < 0 Then
+                Me.Top = 0
+            ElseIf Me.Top + Me.Height > My.Computer.Screen.WorkingArea.Width Then
+                Me.Top = My.Computer.Screen.WorkingArea.Width - Me.Height
+            End If
+
+            If Me.Left < 0 Then
+                Me.Left = 0
+            ElseIf Me.Left + Me.Width > My.Computer.Screen.WorkingArea.Width Then
+                Me.Left = My.Computer.Screen.WorkingArea.Width - Me.Width
+            End If
+            drag = False
+            My.Computer.FileSystem.WriteAllText("C:\Windows\ClockConfig.cfg", Me.Location.X & "," & Me.Location.Y & "," & h12, False)
+        End If
+    End Sub
+
+    Sub bar_hidden()
+        If MousePosition.X > Me.Left Then
+            If MousePosition.Y > Me.Top Then
+                If MousePosition.X < Me.Left + Me.Width Then
+                    If MousePosition.Y < Me.Top + Me.Height Then
+                        P2.Top = 0
+                        Button1.Top = 0
+                        Button2.Top = 0
+                        Button3.Top = 0
+                        PictureBox1.Top = 63
+                        PictureBox2.Top = 63
+                        PictureBox3.Top = 63
+                        PictureBox4.Top = 63
+                        PictureBox5.Top = 63
+                        PictureBox6.Top = 63
+                        Label1.Top = 11
+                        Me.Height = 125
+                    End If
+                End If
+            End If
+        Else
+            P2.Top = 0 - 48
+            Button1.Top = 0 - 48
+            Button2.Top = 0 - 48
+            Button3.Top = 0 - 48
+            PictureBox1.Top = 63 - 48
+            PictureBox2.Top = 63 - 48
+            PictureBox3.Top = 63 - 48
+            PictureBox4.Top = 63 - 48
+            PictureBox5.Top = 63 - 48
+            PictureBox6.Top = 63 - 48
+            Label1.Top = 11 - 48
+            Me.Height = 125 - 48
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If h12 = True Then
+            Button2.Image = My.Resources.h24
+            h12 = False
+        Else
+            Button2.Image = My.Resources.h12
+            h12 = True
+        End If
+        My.Computer.FileSystem.WriteAllText("C:\Windows\ClockConfig.cfg", Me.Location.X & "," & Me.Location.Y & "," & h12, False)
+    End Sub
+End Class
